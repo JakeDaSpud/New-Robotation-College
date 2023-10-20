4b407124b223def0349eeea93e5c7acd331c705c
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2023-10-20 01:16:10 +0100

    Created Proper Player Movement, Added Arm Toggling

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.Animations;
 
@@ -12,13 +13,18 @@ public class PlayerMovement : MonoBehaviour
     public UnityEngine.Vector3 leftRotatePoint;
     public GameObject rightHand;
     public UnityEngine.Vector3 rightRotatePoint;
+    public GameObject playerFacingVector;
 
 
 
     private Rigidbody2D rb;
-    private UnityEngine.Vector2 playerDirection;
     private bool isMoving = false;
-    
+    private bool leftHandActive = false;
+    private bool rightHandActive = false;
+    private UnityEngine.Vector2 playerDirection;
+
+    void Awake() {
+    }    
 
     // Start is called before the first frame update
     void Start()
@@ -29,19 +35,36 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Doc to help me for moving forward: https://discussions.unity.com/t/transform-forward-in-2d/182904
         isMoving = true;
+
+        //Old WASD Movement
+        /*
         float directionX = Input.GetAxisRaw("Horizontal");
         float directionY = Input.GetAxisRaw("Vertical");
 
         playerDirection = new UnityEngine.Vector2(directionX, directionY).normalized;
+        */
+
+        //playerFacingVector is right in front of the player, the x and y are minused from the player's x and y
+        playerDirection = new UnityEngine.Vector2(playerFacingVector.transform.position.x - rb.transform.position.x, playerFacingVector.transform.position.y - rb.transform.position.y).normalized;
 
         if (Input.GetMouseButtonDown(0)) {
-            leftHand.transform.RotateAround(leftRotatePoint, new UnityEngine.Vector3(1, 0, 0), 90);
+            if (leftHandActive) {
+                leftHand.transform.Rotate(new UnityEngine.Vector3(0, 0, 90), 90);
+                leftHandActive = !leftHandActive;
+            }
+
+            else {
+                leftHandActive = !leftHandActive;
+                leftHand.transform.Rotate(new UnityEngine.Vector3(0, 0, 90), -90);
+            }
         }
     }
 
     void FixedUpdate()
     {
+        //Multiplying playerSpeed by the player's front direction
         rb.velocity = new UnityEngine.Vector2(playerDirection.x * playerSpeed, playerDirection.y * playerSpeed);
     }
 }

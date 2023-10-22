74afc571c296f7e039acb873c1ad60ef8280ec71
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2023-10-22 02:09:59 +0100

    Added GameManager, Changed: Pegs, Arm Mechanics, Scene Tilemap

---
+++ Assets/GameManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Game Settings")]
+    public static GameManager instance;
+    public GameObject startButton;
+    public PlayerMovement player;
+
+    [Header("Game States")]
+    public bool isPaused;
+    public bool isGameOver;
+
+    // Awake is called before Start
+    //Singleton pattern
+    void Awake()
+    {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.isDead){
+            RestartGame();
+        }
+    }
+
+    public void PauseGame() {
+        isPaused = true;
+        Time.timeScale = 0.1f;
+        //this is where pause menu goes
+    }
+
+    public void ResumeGame() {
+        isPaused = false;
+        Time.timeScale = 1f;
+        //this is where you deactivate your pause menu
+    }
+
+    public void GameOver() {
+        isGameOver = true;
+        //this is where you decide what happens at game over
+    }
+
+    public void RestartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadMainMenu() {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,35 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.Animations;
+using UnityEngine.Tilemaps;
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Player Settings")]
     public float playerSpeed = 2f;
+    //All old variables for arm bending mechanic
+    /*
     public GameObject leftHand;
     public UnityEngine.Vector3 leftRotatePoint;
     public GameObject rightHand;
     public UnityEngine.Vector3 rightRotatePoint;
-    public GameObject playerFacingVector;
-
+    */
+    public Transform playerFacingVector;
+    public TilemapCollider2D pegCollision;
+    public Color pegShade;
+    public Tilemap pegTilemap;
 
 
     private Rigidbody2D rb;
     private bool isMoving = false;
+    //Old variables for arm bending mechanic
+    /*
     private bool leftHandActive = false;
     private bool rightHandActive = false;
+    */
     private UnityEngine.Vector2 playerDirection;
+    private Color ogPegColour;
+    public bool isDead = false;
 
     void Awake() {
+        
     }    
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        //Setting the variables of the shader colour
+        //Help: https://discussions.unity.com/t/making-sprites-darker-shadowed/122773
+        pegShade.r = 0.6f;
+        pegShade.g = 0.6f;
+        pegShade.b = 0.6f;
+        pegShade.a = pegTilemap.color.a;
+        pegShade.b = pegTilemap.color.b;
+        //This one is so i can set it back to the original peg colour
+        ogPegColour = pegTilemap.color;
     }
 
     // Update is called once per frame
@@ -37,6 +59,8 @@ public class PlayerMovement : MonoBehaviour
     {
         //Doc to help me for moving forward: https://discussions.unity.com/t/transform-forward-in-2d/182904
         isMoving = true;
+
+        OnCollisionEnter2D;
 
         //Old WASD Movement
         /*
@@ -50,6 +74,20 @@ public class PlayerMovement : MonoBehaviour
         playerDirection = new UnityEngine.Vector2(playerFacingVector.transform.position.x - rb.transform.position.x, playerFacingVector.transform.position.y - rb.transform.position.y).normalized;
 
         if (Input.GetMouseButtonDown(0)) {
+            
+            if (pegCollision.enabled) {
+                pegTilemap.color = pegShade;
+            }
+
+            else {
+                pegTilemap.color = ogPegColour;
+            }
+            
+            //Flip whether pegCollision is on
+            pegCollision.enabled = !pegCollision.enabled;
+
+            //Old arm bending system, changed the game mechanic to be pegs changing instead
+            /*
             if (leftHandActive) {
                 leftHand.transform.Rotate(new UnityEngine.Vector3(0, 0, 90), 90);
                 leftHandActive = !leftHandActive;
@@ -59,6 +97,7 @@ public class PlayerMovement : MonoBehaviour
                 leftHandActive = !leftHandActive;
                 leftHand.transform.Rotate(new UnityEngine.Vector3(0, 0, 90), -90);
             }
+            */
         }
     }
 
@@ -66,5 +105,13 @@ public class PlayerMovement : MonoBehaviour
     {
         //Multiplying playerSpeed by the player's front direction
         rb.velocity = new UnityEngine.Vector2(playerDirection.x * playerSpeed, playerDirection.y * playerSpeed);
+    }
+
+    private void OnCollisionEnter2D(TilemapCollider2D other) {
+
+        if (other.tag == "KillBlock") {
+            isDead = true;
+            GameManager.instance.GameOver();
+        }
     }
 }
